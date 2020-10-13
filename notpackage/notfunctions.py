c3119fac1ed860918f349594950bd25075d6d1e7
Silviu Tofan <silviu.tofan@databricks.com>
2020-10-13 05:11:31 +0100

    do library thing too

---
+++ notpackage/notfunctions.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+spark = SparkSession\
+.builder\
+.getOrCreate()
+
+print("Testing simple count")
+
+def spark_f(n_rows):
+    # The Spark code will execute on the Azure Databricks cluster...
+    n = spark.range(n_rows).count()
+    return n
+
+def python_f(n_rows):
+    n = len(range(n_rows))
+    return n
+
+spark_count = spark_f(10000)
+python_count = python_f(9999)
+
+print(spark_count)
+print(python_count)
+
+print("hi, i'm testing this during import")
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='notademo',
-      version='0.0.15',
-      description='A sample PySpark application - 0.0.15',
+      version='0.1.01',
+      description='A sample PySpark application - 0.1.01',
       author='Silviu Tofan',
       author_email='silviu@databricks.com',
       url='www.databricks.com',
-      packages=['mydspackage'],
+      packages=['notpackage'],
       zip_safe=False)
